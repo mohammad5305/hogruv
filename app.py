9893c0f6d98c94d8e4897a22ad2880c7976eccb7
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-11-25 20:54:58 +0330

    feat: prioritizing tasks

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request, redirect, abort
+import jinja2
+from re import sub
 
 app = Flask(__name__)
+
+
+# Custom filter method
+def regex_replace(s, find, replace):
+    """A non-optimal implementation of a regex filter"""
+    return sub(find, replace, s)
+
+
+jinja2.filters.FILTERS['resub'] = regex_replace
 
 
 @app.route('/')
 def home():
     with open('./todo.txt', 'r') as todo:
         todo_txt = todo.readlines()
-
+    todo_txt.sort(key=lambda x: x[1].lower() if x[0] == '-' else 'z')
     return render_template('index.html', todo_txt=todo_txt)
 
 
