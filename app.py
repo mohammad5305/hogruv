6cdc1d171de3118fcf6a513c4f00a50b225fed08
mohammad5305 <mohammad.bajalal546@gmail.com>
2022-11-28 16:23:17 +0330

    feat: a different font for persian

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, abort
 import jinja2
-from re import sub
+import re
 
 app = Flask(__name__)
 
@@ -8,7 +8,20 @@ app = Flask(__name__)
 # Custom filter method
 def regex_replace(s, find, replace):
     """A non-optimal implementation of a regex filter"""
-    return sub(find, replace, s)
+    return re.sub(find, replace, s)
+
+
+def is_persian(string):
+    pattern = r"[\u0600-\u06FF\u0750-\u077F\u0590-\u05FF\uFE70-\uFEFF]"
+
+    return True if re.search(pattern, string) else False
+
+
+def make_para(task):
+    if is_persian(task):
+        return f'<span class="fa">{regex_replace(task, "-[A-Za-z]", "")}</span>'
+    else:
+        return f'<span>{regex_replace(task, "-[A-Za-z]", "")}</span>'
 
 
 jinja2.filters.FILTERS['resub'] = regex_replace
@@ -19,7 +32,7 @@ def home():
     with open('./todo.txt', 'r') as todo:
         todo_txt = todo.readlines()
     todo_txt.sort(key=lambda x: x[1].lower() if x[0] == '-' else 'z')
-    return render_template('index.html', todo_txt=todo_txt)
+    return render_template('index.html', todo_txt=todo_txt, make_para=make_para)
 
 
 @app.route('/add', methods=["GET"])
